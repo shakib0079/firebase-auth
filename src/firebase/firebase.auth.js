ec5869512fb4dceea3a8f923acac8fc03690f008
shakib0079 <shakib100630@gmail.com>
2024-12-16 10:31:57 +0600

    SETTING UP ENV FILE

--- src/firebase/firebase.auth.js
+++ src/firebase/firebase.auth.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDD-xvWt2yw_QusYcgl0zs9dtcmzJfVAZ4",
-  authDomain: "shakibs-world.firebaseapp.com",
-  projectId: "shakibs-world",
-  storageBucket: "shakibs-world.firebasestorage.app",
-  messagingSenderId: "831127276895",
-  appId: "1:831127276895:web:786cec90594eae4efefaea"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
